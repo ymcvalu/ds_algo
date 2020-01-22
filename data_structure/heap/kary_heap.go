7aa5fd22d0a05d970b4d5e9f9e66ed483a22e347
mcll <ymcvalu@gmail.com>
2020-01-22 21:04:54 +0800

    fix bugs

--- data_structure/heap/kary_heap.go
+++ data_structure/heap/kary_heap.go
@@ -177,7 +177,13 @@ func (h *KaryHeap) remove(idx int) interface{} {
 	ln := len(h.array)
 	h.array[idx] = h.array[ln-1]
 	h.array = h.array[:ln-1]
-	h.down(idx)
+
+	if h.cmp(h.array[idx], h.array[(idx-1)/h.k]) {
+		h.up(idx)
+	} else {
+		h.down(idx)
+	}
+
 	return ret
 }
 
